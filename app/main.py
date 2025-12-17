1a2170eaba6df4ede44b355af3b5ec9a7ba032bc
Adan Aamir <adanaamir2005@gmail.com>
2025-12-17 14:16:08 +0500

    testing

--- app/main.py
+++ app/main.py
@@ -141,3 +141,7 @@ def login(email: str, password: str):
     except Exception as e:
         logger.error(f"Login error: {str(e)}")
         raise HTTPException(status_code=500, detail="Login error")
+    
+    
+#YAYYYYYYYYYYYYYYYYY
+#ok
